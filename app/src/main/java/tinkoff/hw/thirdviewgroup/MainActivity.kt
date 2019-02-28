1305eff4597aed5a507186b2f28aafd91b96f10b
Dmitriy Ipatyev <Capucinimo@gmail.com>
2019-03-01 03:27:06 +0500

    allsgood without savestate

--- app/src/main/java/tinkoff/hw/thirdviewgroup/MainActivity.kt
+++ app/src/main/java/tinkoff/hw/thirdviewgroup/MainActivity.kt
@@ -2,64 +2,74 @@ package tinkoff.hw.thirdviewgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.chip.ChipDrawable
 import com.google.android.material.chip.Chip
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_scroll)
+        setContentView(R.layout.activity_main)
         val firstGroup = findViewById<MyViewGroup>(R.id.firstGroup)
         val secondGroup = findViewById<MyViewGroup>(R.id.secondGroup)
-        for (i in 0..20) {
-            //createChip(firstGroup, "Chip$i",secondGroup)
-            createChip(firstGroup, randomWord(15), secondGroup)
+
+        if (savedInstanceState != null) {
+            val chipsTextFirstGroup = savedInstanceState.getStringArrayList("chipsTextFirstGroup")!!
+            val chipsTextSecondGroup = savedInstanceState.getStringArrayList("chipsTextSecondGroup")!!
+            for (chipsText in chipsTextFirstGroup) {
+                createChip(firstGroup, chipsText, secondGroup)
+            }
+            for (chipsText in chipsTextSecondGroup) {
+                createChip(secondGroup, chipsText, firstGroup)
+            }
+        } else {
+            for (i in 0..20) {
+                createChip(firstGroup, randomWord(15), secondGroup)
+            }
+            for (i in 21..60) {
+                createChip(secondGroup, randomWord(15), firstGroup)
+            }
         }
-        for (i in 21..60) {
-            //createChip(secondGroup, "Chip$i",firstGroup)
-            createChip(secondGroup, randomWord(15), firstGroup)
-        }
-        /*val chip = createChip(firstGroup, "textnew",secondGroup)
-        firstGroup.addView(chip)
-        val chip2 = createChip(firstGroup, "chip2",secondGroup)
-        firstGroup.addView(chip2)*/
     }
 
-    private fun randomWord(length:Int): String {
-        return java.util.UUID.randomUUID().toString().substring((36-length..35).random())
+    private fun randomWord(maxLength:Int): String {
+        val length = if (maxLength>36 || maxLength <1) 15 else maxLength
+        return java.util.UUID.randomUUID().toString().substring((36 - length..35).random())
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
+        val firstGroup = findViewById<MyViewGroup>(R.id.firstGroup)
+        val secondGroup = findViewById<MyViewGroup>(R.id.secondGroup)
+        saveChipsText(firstGroup,outState,"chipsTextFirstGroup")
+        saveChipsText(secondGroup,outState,"chipsTextSecondGroup")
     }
 
+    private fun saveChipsText(myViewGroup: MyViewGroup, outState: Bundle,key: String? ) {
+        val chipsTextMyViewGroup: ArrayList<String> = ArrayList()
+        for (i in 0 until myViewGroup.childCount) {
+            val currentChip = myViewGroup.getChildAt(i) as Chip
+            chipsTextMyViewGroup.add(currentChip.chipText.toString())
+        }
+        outState.putStringArrayList(key, chipsTextMyViewGroup)
+    }
 
     private fun createChip(entryViewGroup: ViewGroup, text: String, newViewGroup: ViewGroup) {
         val chip = Chip(this)
         chip.setChipDrawable(ChipDrawable.createFromResource(this, R.xml.chip))
         //val chip = LayoutInflater.from(this).inflate(R.layout.chip, entryViewGroup, false)
         chip.chipText = text
-        chip.id = View.generateViewId()
-        chip.setOnCloseIconClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                if (chip.parent == entryViewGroup) {
-                    entryViewGroup.removeView(chip)
-                    newViewGroup.addView(chip)
-                } else if (chip.parent == newViewGroup) {
-                    newViewGroup.removeView(chip)
-                    entryViewGroup.addView(chip)
-                }
+        //chip.id = View.generateViewId()
+        chip.setOnCloseIconClickListener {
+            if (chip.parent == entryViewGroup) {
+                entryViewGroup.removeView(chip)
+                newViewGroup.addView(chip)
+            } else if (chip.parent == newViewGroup) {
+                newViewGroup.removeView(chip)
+                entryViewGroup.addView(chip)
             }
-        })
+        }
         entryViewGroup.addView(chip)
     }
 }
--- app/src/main/java/tinkoff/hw/thirdviewgroup/MyViewGroup.kt
+++ app/src/main/java/tinkoff/hw/thirdviewgroup/MyViewGroup.kt
@@ -1,13 +1,9 @@
 package tinkoff.hw.thirdviewgroup
 
 import android.content.Context
-import android.os.Parcelable
 import android.util.AttributeSet
-import android.util.SparseArray
 import android.view.ViewGroup
-import android.os.Parcel
-import android.view.View
-
+import android.view.Gravity
 
 class MyViewGroup : ViewGroup {
 
@@ -15,7 +11,7 @@ class MyViewGroup : ViewGroup {
     var paddingWidth = 0
     var paddingHeight = 0
     var childHeight = 0
-    var gravity:String? = "left"
+    var gravity = Gravity.LEFT
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         if (attrs != null) {
@@ -28,121 +24,12 @@ class MyViewGroup : ViewGroup {
         }
     }
 
-    /*public override fun onSaveInstanceState(): Parcelable? {
-        val superState = super.onSaveInstanceState()
-        val ss = SavedState(superState)
-        ss.childrenStates = SparseArray<Parcelable>()
-        for (i in 0 until childCount) {
-            getChildAt(i).saveHierarchyState(ss.childrenStates as SparseArray<Parcelable>)
-        }
-        return ss
-    }
-
-    public override fun onRestoreInstanceState(state: Parcelable) {
-        val ss = state as SavedState
-        super.onRestoreInstanceState(ss.superState)
-        for (i in 0 until childCount) {
-            getChildAt(i).restoreHierarchyState(ss.childrenStates as SparseArray<Parcelable>)
-        }
-    }
-
-    override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>) {
-        dispatchFreezeSelfOnly(container)
-    }
-
-    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>) {
-        dispatchThawSelfOnly(container)
-    }
-
-    internal class SavedState : View.BaseSavedState {
-        var childrenStates: SparseArray<Parcelable>? = null
-
-        constructor(superState: Parcelable) : super(superState) {}
-
-        private constructor(`in`: Parcel, classLoader: ClassLoader) : super(`in`) {
-            childrenStates = `in`.readSparseArray(classLoader) as SparseArray<Parcelable>
-        }
-
-        override fun writeToParcel(out: Parcel, flags: Int) {
-            super.writeToParcel(out, flags)
-            out.writeSparseArray(childrenStates as SparseArray<Any>)
-        }
-
-        companion object {
-@JvmField
-            val CREATOR: Parcelable.ClassLoaderCreator<SavedState> =
-                object : Parcelable.ClassLoaderCreator<SavedState> {
-                    override fun createFromParcel(source: Parcel, loader: ClassLoader?): SavedState {
-                        return SavedState(source, loader!!)
-                    }
-
-                    override fun createFromParcel(source: Parcel): SavedState {
-                        return createFromParcel(source, null)
-                    }
-
-                    override fun newArray(size: Int): Array<SavedState?> {
-                        return arrayOfNulls(size)
-                    }
-                }
-        }
-    }*/
-    /*override fun onSaveInstanceState(): Parcelable {
-        val saveInstanceState = super.onSaveInstanceState()
-        val savedState = SavedState(saveInstanceState)
-        savedState.childrenStates = SparseArray()
-        for (i in 0 until childCount) {
-            getChildAt(i).saveHierarchyState(savedState.childrenStates)
-        }
-        return savedState
-    }
-
-    override fun onRestoreInstanceState(state: Parcelable) {
-        val savedState = state as SavedState
-        super.onRestoreInstanceState(savedState.getSuperState())
-        for (i in 0 until childCount) {
-            getChildAt(i).restoreHierarchyState(savedState.childrenStates)
-        }
-    }
-
-    private class SavedState : View.BaseSavedState {
-        var childrenStates: SparseArray<Any>? = null
-
-        internal constructor(superState: Parcelable) : super(superState)
-
-        private constructor(`in`: Parcel, classLoader: ClassLoader) : super(`in`) {
-            childrenStates = `in`.readSparseArray(classLoader)
-        }
-
-        override fun writeToParcel(out: Parcel, flags: Int) {
-            super.writeToParcel(out, flags)
-            out.writeSparseArray(childrenStates)
-        }
-
-        companion object {
-
-            val CREATOR: Parcelable.ClassLoaderCreator<SavedState> =
-                object : Parcelable.ClassLoaderCreator<SavedState> {
-                    override fun createFromParcel(source: Parcel, loader: ClassLoader): SavedState {
-                        return SavedState(source, loader)
-                    }
-
-                    override fun createFromParcel(source: Parcel?): SavedState {
-                        return createFromParcel(null)
-                    }
-
-                    override fun newArray(size: Int): Array<SavedState> {
-                        return arrayOfNulls(size)
-                    }
-                }
-        }
-    }*/
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val width = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
-        val height = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
+        val width = MeasureSpec.getSize(widthMeasureSpec) - paddingRight
+        val height = MeasureSpec.getSize(heightMeasureSpec)
         val count = childCount
-        var xpos = paddingLeft
-        var ypos = paddingTop
+        var xPosition = paddingLeft
+        var yPosition = paddingTop
         val childHeightMeasureSpec= MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY)
         val childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(width,MeasureSpec.AT_MOST)
         val stringHeight = childHeight + paddingHeight
@@ -151,77 +38,71 @@ class MyViewGroup : ViewGroup {
             val child = getChildAt(i)
             if (child.visibility != GONE) {
                 child.measure(childWidthMeasureSpec, childHeightMeasureSpec)
-                val childw = child.measuredWidth
-                if (xpos + childw > width) {
-                    xpos = paddingLeft
-                    ypos += stringHeight
+                val childWidth = child.measuredWidth
+                if (xPosition + childWidth > width) {
+                    xPosition = paddingLeft
+                    yPosition += stringHeight
                     lastViews.add(i-1)
                 }
-                xpos += childw + paddingWidth
+                xPosition += childWidth + paddingWidth
             }
         }
         var resultHeight = 0
         when (MeasureSpec.getMode(heightMeasureSpec)) {
-            MeasureSpec.UNSPECIFIED -> resultHeight = ypos + stringHeight
-            MeasureSpec.AT_MOST -> resultHeight = if (ypos + stringHeight < height) ypos + stringHeight else height
+            MeasureSpec.UNSPECIFIED -> resultHeight = yPosition + stringHeight + paddingBottom
+            MeasureSpec.AT_MOST -> resultHeight = if (yPosition + stringHeight < height) yPosition + stringHeight + paddingBottom else height
             MeasureSpec.EXACTLY -> resultHeight = height
         }
-        setMeasuredDimension(width, resultHeight)
+        setMeasuredDimension(width + paddingRight, resultHeight)
     }
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val width = r - l
-        var xpos = paddingLeft
-        var ypos = paddingTop
-        if (gravity == "left") for (i in 0 until childCount) {
+        val width = r - l - paddingRight
+        var xPosition = paddingLeft
+        var yPosition = paddingTop
+        if (gravity != Gravity.RIGHT) for (i in 0 until childCount) {
             val child = getChildAt(i)
             if (child.visibility != GONE) {
-                val childw = child.measuredWidth
-                val childh = child.measuredHeight
-                if (xpos + childw > width) {
-                    xpos = paddingLeft
-                    ypos += childHeight + paddingHeight
+                val childWidth = child.measuredWidth
+                val childHeight = child.measuredHeight
+                if (xPosition + childWidth > width) {
+                    xPosition = paddingLeft
+                    yPosition += this.childHeight + paddingHeight
                 }
-                child.layout(xpos, ypos, xpos + childw, ypos + childh)
-                xpos += childw + paddingWidth
+                child.layout(xPosition, yPosition, xPosition + childWidth, yPosition + childHeight)
+                xPosition += childWidth + paddingWidth
             }
         }
         else {
-
-
             for (i in 0 until lastViews.size) {
-                xpos = width - paddingRight
+                xPosition = width
                 for (j in lastViews[i] downTo if (i == 0) 0 else lastViews[i - 1] + 1) {
                     val child = getChildAt(j)
                     if (child.visibility != GONE) {
-                        val childw = child.measuredWidth
-                        child.layout(xpos- childw, ypos, xpos, ypos + childHeight)
-                        xpos -= childw + paddingWidth
+                        val childWidth = child.measuredWidth
+                        child.layout(xPosition - childWidth, yPosition, xPosition, yPosition + childHeight)
+                        xPosition -= childWidth + paddingWidth
                     }
                 }
-                ypos += childHeight + paddingHeight
-
-
+                yPosition += childHeight + paddingHeight
             }
-            xpos = width - paddingRight
-            val firstElement= if (lastViews.isEmpty()) 0
-            else lastViews.last()+1
-            for (i in childCount-1 downTo firstElement) {
+            xPosition = width
+            val firstElement = if (lastViews.isEmpty()) 0 else lastViews.last() + 1
+            for (i in childCount - 1 downTo firstElement) {
                 val child = getChildAt(i)
                 if (child.visibility != GONE) {
-                    val childw = child.measuredWidth
-                    child.layout(xpos- childw, ypos, xpos, ypos + childHeight)
-                    xpos -= childw + paddingWidth
+                    val childWidth = child.measuredWidth
+                    child.layout(xPosition - childWidth, yPosition, xPosition, yPosition + childHeight)
+                    xPosition -= childWidth + paddingWidth
                 }
             }
         }
-
     }
     private fun setAttributes(context:Context, attrs: AttributeSet) {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyViewGroup)
         paddingWidth = typedArray.getInt(R.styleable.MyViewGroup_padding_width, 0)
         paddingHeight = typedArray.getInt(R.styleable.MyViewGroup_padding_height, 0)
         childHeight = typedArray.getInt(R.styleable.MyViewGroup_child_height, 50)
-        gravity = if (typedArray.getString(R.styleable.MyViewGroup_gravity) == null) "left" else typedArray.getString(R.styleable.MyViewGroup_gravity)
+        gravity = typedArray.getInt(R.styleable.MyViewGroup_android_gravity, Gravity.LEFT)
         typedArray.recycle()
     }
 }
